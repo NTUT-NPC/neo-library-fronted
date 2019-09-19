7ea040fbf366936830ce54250eed134eadafd341
DevilTea <ch870814@gmail.com>
2019-09-20 01:35:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-responsive :aspect-ratio="9/16"></v-responsive>
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: black;
+}
+
+.fill-parent {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 deviltea
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+module.exports = {
+  devServer: {
+    // Enable dev server to accept the external requests.
+    disableHostCheck: true
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/neo-library-frontend/'
+    : '/'
+}
